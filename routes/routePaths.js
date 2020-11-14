7b7dc12cfdc2169e2f4bd800247175d5f1492922
LiamRotchford5671 <liamrotchford@gmail.com>
2020-11-13 16:52:02 -0800

    Initial Trefle API set up test

--- routes/routePaths.js
+++ routes/routePaths.js
@@ -1,9 +1,26 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const axois = require('axios');
+
+
+const url = 'https://trefle.io/api/v1/plants?token=' + `${token}`;
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('homepage');
+  res.end();
+});
+
+//API main fetch (only first page (30 results returned))
+router.get('/searchResults', async (req, res) => {
+
+  let dataRes = [];
+  const query = await axois.get(url)
+    .then(resp => console.log(resp.data.length))
+    .catch(err => console.error(err));
+
+
+  res.end();
 });
 
 module.exports = router;
