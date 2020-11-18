1eff23398ff8a9a44e0759f9c738bcc2e28fec67
LiamRotchford5671 <liamrotchford@gmail.com>
2020-11-18 15:18:12 -0800

    Adjusted the routes to send Json data to views
    -Static json data can now be accessed and used via EJS embeded js tags for the carousel
    -set up the data renders for the API calls on the results page

--- routes/routePaths.js
+++ routes/routePaths.js
@@ -3,6 +3,8 @@ const app = express();
 const router = express.Router();
 const axois = require('axios');
 const url = require('url');
+const fs = require('fs');
+
 
 const token = process.env.API_TOKEN;
 const urlAPI = 'https://trefle.io/api/v1/';
@@ -11,7 +13,13 @@ app.use(express.static('public'))
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('homepage');
+  let sgtdata = fs.readFileSync('./public/json/suggestionsData.json');
+  let sgtObj = JSON.parse(sgtdata);
+
+  res.render('homepage', {
+    data: sgtObj
+  });
+
   res.end();
 });
 
@@ -24,13 +32,21 @@ router.get('/results', async (req, res) => {
   let searchStr = Object.values(queryObject);;
 
   if (determine == 'genus') {
-    const query = await axois.get(urlAPI + 'genus/' + searchStr + '/plants?token=' + token + '&genus=' + searchStr)
+    let genusObj = await axois.get(urlAPI + 'genus/' + searchStr + '/plants?token=' + token + '&genus=' + searchStr)
       .then(resp => console.log(resp.data))
       .catch(err => console.error(err));
+
+    res.render('results', {
+      data: genusObj
+    });
   } else {
-    const query = await axois.get(urlAPI + 'plants/search?token=' + token + '&q=' + searchStr)
+    let searchObj = await axois.get(urlAPI + 'plants/search?token=' + token + '&q=' + searchStr)
       .then(resp => console.log(resp.data))
       .catch(err => console.error(err));
+
+    res.render('results', {
+      data: searchObj
+    });
   }
 
   res.end();
