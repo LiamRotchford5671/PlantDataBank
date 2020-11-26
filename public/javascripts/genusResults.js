f04ac6c162599b7fc857591bb0d614d7ea5623e3
LiamRotchford5671 <liamrotchford@gmail.com>
2020-11-25 17:19:49 -0800

    Adjusted the results pages
    -included a previous button that will go back to the previous page of results
    -reworked the JS and routePaths to handle that functionailty
    -no longer a werid (No more results) page when you go past the last page
    -removed old EJS logic on the pages and reworked it to be simpler & work with the previous buttons
    -No previous button on first page, no next button on the last page
    -Due to logic i had to create the next button when your on the last page so the JS still works but it's hidden and disabled so you can't accidently click on where the button might be and activate it

--- public/javascripts/genusResults.js
+++ public/javascripts/genusResults.js
@@ -7,8 +7,21 @@ nextGenusBtn.addEventListener('click', event => {
     let searchStr = destruct[0].split('=');
     let pageNum = nextGenusBtn.value;
 
-    if (pageNum !== 'reset') {
-        pageNum++;
+    pageNum++;
+    window.location.href = 'genusResults?nextGenusPage=' + searchStr[1] + '&page=' + pageNum;
+});
+
+/* Previous Button Genus Results */
+const prevGenusBtn = document.getElementById("prevGenusBtn");
+
+prevGenusBtn.addEventListener('click', event => {
+    let query = window.location.search.substring(1).split('?');
+    let destruct = query[0].split('&');
+    let searchStr = destruct[0].split('=');
+    let pageNum = prevGenusBtn.value;
+
+    if (pageNum != 2) {
+        pageNum--;
         window.location.href = 'genusResults?nextGenusPage=' + searchStr[1] + '&page=' + pageNum;
     } else {
         window.location.href = 'genusResults?genus=' + searchStr[1];
