836cd9363d099e77a28af684e5ea51854f1e919b
snakevirgo <iris.ngarden@gmail.com>
2020-11-27 16:44:39 -0800

    footer in progress and styling

--- public/javascripts/genusResults.js
+++ public/javascripts/genusResults.js
@@ -32,3 +32,20 @@ prevGenusBtn.addEventListener('click', event => {
 function getDetails(slug) {
     window.location.href = 'plantInformation?plant=' + slug;
 }
+
+// Nav bar
+const navSlide = function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    console.log("it has run");
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+    })
+}
+
+navSlide();
